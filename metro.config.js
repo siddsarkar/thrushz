7a4881208b49e2f52a387ed545bd41f6d3ac4be1
Siddhartha Sarkar <siddsarkar7@gmail.com>
2025-12-28 21:05:14 +0530

    feat: update project configuration and dependencies; integrate Sentry for error tracking

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+const {
+  getSentryExpoConfig
+} = require("@sentry/react-native/metro");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getSentryExpoConfig(__dirname);
+config.resolver.sourceExts.push('sql');
+
+module.exports = config;
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,35 @@
-import { Stack } from "expo-router";
+import * as Sentry from '@sentry/react-native';
+import { drizzle } from 'drizzle-orm/expo-sqlite';
+import { Stack } from 'expo-router';
+import * as SQLite from 'expo-sqlite';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://ee982592402fa1ed01fc7c47ada8bdc0@o4506185854156800.ingest.us.sentry.io/4509555266486272',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+const expo = SQLite.openDatabaseSync('db.db');
+const db = drizzle(expo);
+
+function RootLayout() {
   return <Stack />;
 }
+
+export default Sentry.wrap(RootLayout);
